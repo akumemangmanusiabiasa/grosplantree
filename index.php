b3367ad271de60616260b3533896708ac0a1e10d
Mochammad Anwar Fauzi <nericnerica@gmail.com>
2023-11-14 11:27:10 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    
+    
+    <title>Welcome to Grosplan Link Tree</title>
+
+    <!-- css font awesome -->
+    <link rel="stylesheet" type="text/css" href="css/all.css">
+    <!-- icon project -->
+    <link rel="icon" href="img/icon.png" sizes="32x32" type="image/png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/w3.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+        <!-- Alpha.ly Version 0.1 | Development Version -->
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center" style="padding-top: 30px; padding-bottom: 30px;">
+                <img class="backdrop linktree">
+                <!-- <h2 style="color: #ffffff; padding-top: 20px;">linktree<br>Never Ending Stories</h2> -->
+            </div>
+
+    </div>
+    </div>
+
+
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center">
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='files/GROSPLAN-ID.pdf'" type="button" class="btn btn-outline-light" style="width: 30%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><i class="fa-solid fa-clipboard-list fa-fade"></i></i><a>   Price List</a></button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://wa.link/xu2hj3'" type="button" class="btn btn-outline-light" style="width: 30%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><i class="fa-brands fa-whatsapp fa-fade"></i><a>   Whatsapp Grosplan</a></button>
+                </div>
+                
+            </div>
+    </div>
+    </div>
+
+        
+        <div class="w3-center w3-padding-24">Powered by 
+            <a title=">-- © Grosplan -- 2022 --<" target="_blank" class="w3-hover-opacity">  Grosplan DEV -- © 2022 |</a><br>| <img src="img/LogoGrosplan_White.png" alt="icon" class="w3-image" width="128" height="72">|
+        </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/w3.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- js font awesome -->
+    <script type="text/javascript" src="js/all.js"></script>
+    
+    <!-- This pre-lander was created by Brandon Nilsson -->
+    <!-- You're welcome to edit, reproduce and change this template as long as original contributation stays present on the website at all times.  -->
+</body>
+
+</html>
